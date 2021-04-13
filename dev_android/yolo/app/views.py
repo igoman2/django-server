a1c62cb181dc0878d408a8495789a5aae60e56c9
han <igoman2@ajou.ac.kr>
2021-04-14 01:07:36 +0900

    django env setting

---
+++ dev_android/yolo/app/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serializers import QuizSerializer
+
+
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("hello world!")
+# Create your views here.
